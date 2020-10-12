b7998972ab3704aede5ae3f1a04ead994ccbc950
harry <harry@atguigu.com>
2020-10-12 16:57:09 +0800

    day01

---
+++ gmall-logger/src/main/java/com/atguigu/gmalllogger/controller/LoggerController.java
@@ -0,0 +1,60 @@
+package com.atguigu.gmalllogger.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import com.atguigu.constants.GmallConstant;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@RestController = @Controller + @ResponseBody
+@Slf4j
+public class LoggerController {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @RequestMapping("test1")
+    //@ResponseBody  //将当前方法改为返回字符串,而不是页面
+    public String test1() {
+        System.out.println("***********");
+        return "success";
+    }
+
+    @RequestMapping("test2")
+    public String test2(@RequestParam("name") String nn,
+                        @RequestParam("age") int age) {
+        System.out.println(nn + ":" + age);
+        return "success";
+    }
+
+    @RequestMapping("log")
+    public String getLog(@RequestParam("logString") String logStr) {
+
+        //1.添加时间戳
+        JSONObject jsonObject = JSONObject.parseObject(logStr);
+        jsonObject.put("ts", System.currentTimeMillis());
+
+        String addTsLogStr = jsonObject.toString();
+
+        //2.将数据写入日志
+        log.info(addTsLogStr);
+
+        //3.将数据写入Kafka
+        if ("startup".equals(jsonObject.getString("type"))) {
+            kafkaTemplate.send(GmallConstant.KAFKA_TOPIC_START, addTsLogStr);
+        } else {
+            kafkaTemplate.send(GmallConstant.KAFKA_TOPIC_EVENT, addTsLogStr);
+        }
+
+        //4.返回
+        return "success";
+    }
+}
